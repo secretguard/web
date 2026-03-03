301e9da71aada3645219297a770871e1dfcab60d
Sarath G <57167283+secretguard@users.noreply.github.com>
2026-03-04 03:51:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-/* 
-  All custom logic (GSAP Animations, Maps, Contact Form) 
-  has been integrated directly into the bottom of index.html 
-  to unify the dependencies and make the single-page portfolio 
-  easier to manage. 
-*/
