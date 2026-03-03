f506505e5fb5eadcc2ebcc2188bdf0a8de60027f
secretguard <sarathkarakkad1@gmail.com>
2026-03-04 03:45:58 +0530

    Initialize and refine portfolio website

---
+++ script.js
@@ -0,0 +1,6 @@
+/* 
+  All custom logic (GSAP Animations, Maps, Contact Form) 
+  has been integrated directly into the bottom of index.html 
+  to unify the dependencies and make the single-page portfolio 
+  easier to manage. 
+*/
